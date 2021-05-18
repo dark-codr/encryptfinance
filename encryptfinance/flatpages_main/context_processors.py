9a960206da4edf69f5466a94b96d57b54aa00ce4
jeremiah david <jeremiahedavids@gmail.com>
2021-05-18 23:38:20 +0100

    fixed and added faq and flatpages

--- encryptfinance/flatpages_main/context_processors.py
+++ encryptfinance/flatpages_main/context_processors.py
@@ -2,5 +2,5 @@ from django.utils import timezone
 from .models import FAQ
 
 def recent_faq(request):
-    faqs = FAQ.objects.all()[:7]
+    faqs = FAQ.objects.all()[:15]
     return {'recent_faq':faqs}
