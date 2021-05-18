c89611c8f12144c1b95e096fd4e05eef278cea5e
jeremiah david <jeremiahedavids@gmail.com>
2021-05-18 06:59:14 +0100

    fixed card bg color

--- encryptfinance/users/context_processors.py
+++ encryptfinance/users/context_processors.py
@@ -2,5 +2,5 @@ from django.utils import timezone
 from .models import Testimonial
 
 def recent_testimonials(request):
-    testimonials = Testimonial.objects.all().filter(modified__lte=timezone.now())
+    testimonials = Testimonial.objects.all().filter(modified__lte=timezone.now())[:7]
     return {'recent_testimonials':testimonials}
